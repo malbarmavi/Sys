5889fceec671f47b7b92ca67c88d5f480cead67f
mbarmawi <muhammadbarmawi@outlook.com>
2016-04-29 11:52:06 -0800

    Initial implementation

---
+++ Sys/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+using static Sys.Utility;
+using Helper;
+namespace Sys
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string command = string.Empty;
+      Title = "Sys";
+      WriteLine("Sys console info command version 0.1 beta.\nType help to get the availabel commands.\n");
+      while (command != "exit")
+      {
+        Write(">");
+        command = ReadLine();
+
+        switch (command.ToLower())
+        {
+          case "clear":
+          case "cls":
+            Clear(); // clear the console
+            break;
+          case "os":
+            GetInfo(SystemInfo.GetOperatingSystems());
+            break;
+          case "ua":
+          case "users-accounts":
+            GetInfo(SystemInfo.GetUserAccounts());
+            break;
+          case "sa":
+          case "system-accounts":
+            GetInfo(SystemInfo.GetUserAccounts());
+            break;
+          case "cpu":
+            GetInfo(SystemInfo.GetCPU());
+            break;
+          case "mem":
+          case "memory":
+            GetInfo(SystemInfo.GetMemory());
+            break;
+          case "dd":
+          case "disks-drives":
+            GetInfo(SystemInfo.GetDiskDrives());
+            break;
+          case "dp":
+          case "disks-partition":
+            GetInfo(SystemInfo.GetDiskPartition());
+            break;
+          case "ld":
+          case "logical-disk":
+            GetInfo(SystemInfo.GetLogicalDisks());
+            break;
+          case "mb":
+          case "motherboard":
+            GetInfo(SystemInfo.GetMotherBoard());
+            break;
+          case "bois":
+            GetInfo(SystemInfo.GetBios());
+            break;
+          case "about":
+          case "help":
+            WriteLine("\n Command                    Alias");
+            WriteLine(" --------------------------------");
+            WriteLine(" os                            ");
+            WriteLine(" users-accounts             ua   ");
+            WriteLine(" system-accounts            sa   ");
+            WriteLine(" cpu                           ");
+            WriteLine(" memory                     mem  ");
+            WriteLine(" disks-drives               dd   ");
+            WriteLine(" disks-partition            dp   ");
+            WriteLine(" logical-disk               ld   ");
+            WriteLine(" motherboard                mb   ");
+            WriteLine(" bois                          ");
+            WriteLine(" about                         \n");
+            break;
+          default:
+            if (command != "")
+            {
+              WriteLine("Bad command or file not found.");
+            }
+            break;
+        }
+
+      }
+
+    }
+  }
+}
---
+++ Sys/Utility.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Sys
+{
+  using Helper.Extention;
+  public static class Utility
+  {
+    public static void GetInfo<T>(List<T> infoList)
+    {
+      WriteLine("");
+      foreach (var info in infoList)
+      {
+        GetInfo(info);
+      }
+      WriteLine("");
+    }
+
+    public static void GetInfo(object info)
+    {
+      foreach (var prop in info.GetType().GetProperties())
+      {
+        if (prop.GetValue(info).ToString().IsValidString())
+        {
+          WriteLine($" {FormateMemberName(prop.Name).PadRight(25)}: {prop.GetValue(info).ToString().PadRight(5)}");
+        }
+      }
+      WriteLine("");
+
+    }
+    /// <summary>
+    /// put a space before capital leter eg AccountName => Account Name
+    /// </summary>
+    /// <param name="memberName">Name of member</param>
+    /// <returns></returns>
+    public static string FormateMemberName(string memberName) => memberName.Select(c => char.IsUpper(c) ? $" {c}" : c.ToString()).Aggregate((sum, i) => $"{sum}{i}");
+
+  }
+}
